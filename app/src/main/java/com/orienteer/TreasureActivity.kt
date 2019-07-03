eec944008549c28087a5faa337d11cf04617f83a
James Gliwa <james.gliwa@epicgames.com>
2019-07-03 14:16:53 -0700

    back button on action bar to main activity

--- app/src/main/java/com/orienteer/TreasureActivity.kt
+++ app/src/main/java/com/orienteer/TreasureActivity.kt
@@ -2,6 +2,7 @@ package com.orienteer
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
@@ -21,5 +22,20 @@ class TreasureActivity : AppCompatActivity() {
         // Set up the action bar
         val toolbar: Toolbar = binding.toolbarInclude.toolbar
         setSupportActionBar(toolbar)
+
+        // Set the action bar to have a back button showing
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Destroy this activity and return to previous activity on the stack.
+        if (item.itemId == android.R.id.home)
+        {
+            this.finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
