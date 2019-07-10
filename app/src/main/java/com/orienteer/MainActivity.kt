48b03f2bf17e9bdb84a8e4555c7ad5d07b3219ad
James Gliwa <jgliwa12@gmail.com>
2019-07-09 22:47:16 -0700

    refactor done

--- app/src/main/java/com/orienteer/MainActivity.kt
+++ app/src/main/java/com/orienteer/MainActivity.kt
@@ -1,181 +1,74 @@
 package com.orienteer
 
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.navigation.NavigationView
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.orienteer.databinding.ActivityMainBinding
-import com.orienteer.map.MapViewModel
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding : ActivityMainBinding
 
-    private lateinit var _fusedLocationProviderClient: FusedLocationProviderClient
-
-    /**
-     * Lazily initialize our [MapViewModel].
-     */
-    private val mapViewModel: MapViewModel by lazy {
-        ViewModelProviders.of(this).get(MapViewModel::class.java)
-    }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment_new_main)
 
-        // Giving the binding access to the MapViewModel
-        binding.mapViewModel = mapViewModel
+        // Set up the app bar to use the nav graph and drawer
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
 
-        // Set map callback on the mapView
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.mapFragment) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-
-        _fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
+        // Set up the toolbar
         val toolbar: Toolbar = binding.toolbarInclude.toolbar
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener {
-            Log.i("FAB", "clicked the floating action button")
-            // start the treasure activity with its own nav graph
-            startActivity(Intent(this, TreasureActivity::class.java).apply {
-                putExtra("Test", "test_message")
-            })
+        // This gets the header view at the first position of the NavigationView, which is the correct
+        // header position that we want.
+        binding.navView.getHeaderView(0).setOnClickListener {
+            // TODO: Can we get this from a binding?
+            navController.navigate(R.id.profileFragment)
+            binding.drawerLayout.closeDrawers()
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = binding.navView
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
 
-
-        toggle.syncState()
-    }
-
-    override fun onBackPressed() {
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(map: GoogleMap) {
-        // Set the map in the viewModel
-        mapViewModel.setMap(map)
-
-        // Check Permissions
-        getLocationPermission()
-
-        // Update the Map UI
-        mapViewModel.updateLocationUI()
-
-        // Get Device Location
-        getDeviceLocation()
-    }
-
-    /**
-     * Request location permission, so that we can get the location of the
-     * device. The result of the permission request is handled by a callback,
-     * onRequestPermissionsResult.
-     */
-    private fun getLocationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this.applicationContext,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            mapViewModel.setLocationPermissionGranted(true)
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                mapViewModel.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
-        }
-    }
-
-    /**
-     * Handles the result of the request for location permissions.
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        mapViewModel.setLocationPermissionGranted(false)
-        when (requestCode) {
-            mapViewModel.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mapViewModel.setLocationPermissionGranted(true)
-                }
+        // Only allow the drawer on the top level map fragment.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id != R.id.mapFragment) {
+                binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            } else {
+                binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
             }
         }
-        mapViewModel.updateLocationUI()
-        getDeviceLocation()
+
+        binding.toolbarInclude.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
 
-    /**
-     * Get the best and most recent location of the device, which may be null in rare
-     * cases when a location is not available.
-     */
-    private fun getDeviceLocation() {
-        try {
-            if (mapViewModel.locationPermissionGranted.value!!) {
-                val locationResult = _fusedLocationProviderClient.lastLocation
-                locationResult.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Set the map's camera position to the current location of the device.
-                        mapViewModel.setLastKnownLocation(task.result as Location)
-                        mapViewModel.updateMap()
-                    } else {
-                        Log.d("MapViewModel", "Current location is null. Using defaults.")
-                        Log.e("MapViewModel", "Exception: %s", task.exception)
-                        mapViewModel.resetMap()
-                        mapViewModel.disableMyLocationButtonEnabled()
-                    }
-                }
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message)
-        }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //
+        // Using the onNavDestinationSelected helper requires the menu
+        // item id and the navigation destination id to match in order
+        // for this to work.
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
 
     }
 }
