79ad21d4def8ec9a0a6cf646909ebd2551a6669f
James Gliwa <jgliwa12@gmail.com>
2019-08-09 16:23:20 -0700

    adding a pagersnaphelper to the recyclerview

--- app/src/main/java/com/orienteer/map/MapFragment.kt
+++ app/src/main/java/com/orienteer/map/MapFragment.kt
@@ -15,6 +15,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -79,6 +80,9 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             Toast.makeText(context, "Clicked treasure hunt ${it.name}!", Toast.LENGTH_LONG).show()
         })
 
+        // Set the snap helper so that the cards snap to the full screen on swiping
+        val snapHelper = PagerSnapHelper() // Or PagerSnapHelper
+        snapHelper.attachToRecyclerView(binding.treasureHuntsCardsMap)
 
         return binding.root
     }
