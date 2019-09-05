fff4d0ddd275d0dac73e3be95dce3ee0ccf33bc6
James Gliwa <jgliwa12@gmail.com>
2019-09-04 22:39:14 -0700

    adding wiring for the followed adventures screen

---
+++ app/src/main/java/com/orienteer/treasurehunts/AdventuresFollowedFragment.kt
@@ -0,0 +1,49 @@
+package com.orienteer.treasurehunts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.orienteer.databinding.FragmentAdventuresFollowedBinding
+
+class AdventuresFollowedFragment : Fragment() {
+
+    /**
+     * Lazily initialize our [AdventuresFollowedViewModel].
+     */
+    private val viewModel: AdventuresFollowedViewModel by lazy {
+        ViewModelProviders.of(this).get(AdventuresFollowedViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentAdventuresFollowedBinding.inflate(inflater)
+
+        binding.viewModel = viewModel
+
+        //TODO: make this use the list returned from the viewmodel
+//        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+//        binding.lifecycleOwner = this
+//
+//        binding.treasureHuntsCardList.adapter = TreasureHuntsAdapter(TreasureHuntsAdapter.OnClickListener {
+//            Toast.makeText(context, "Clicked treasure hunt ${it.name}!", Toast.LENGTH_LONG).show()
+//            viewModel.displayTreasureHuntDetails(it)
+//        })
+//
+//        // Observe the navigateToSelectedProperty LiveData and Navigate when it isn't null
+//        // After navigating, call displayPropertyDetailsComplete() so that the ViewModel is ready
+//        // for another navigation event.
+//        viewModel.navigateToSelectedTreasureHunt.observe(this, Observer {
+//            if ( null != it ) {
+//                // Must find the NavController from the Fragment
+//                this.findNavController().navigate(
+//                    TreasureHuntsFragmentDirections.actionTreasureHuntsFragmentToTreasureHuntDetail(it))
+//
+//                // Tell the ViewModel we've made the navigate call to prevent multiple navigation
+//                viewModel.doneNavigatingToSelectedTreasureHunt()
+//            }
+//        })
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/orienteer/treasurehunts/AdventuresFollowedViewModel.kt
@@ -0,0 +1,10 @@
+package com.orienteer.treasurehunts
+
+import androidx.lifecycle.ViewModel
+
+class AdventuresFollowedViewModel : ViewModel() {
+
+    init {
+        //TODO: call the service to retrieve these
+    }
+}
