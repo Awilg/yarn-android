1b9d5f6c4e3c296b0af2c26265f507feca2c3d8c
James Gliwa <jgliwa12@gmail.com>
2019-12-20 13:20:19 -0500

    Adding firebase ml labeling

--- app/src/main/java/com/orienteer/treasurecreate/ClueTypePhotoCreateFragment.kt
+++ app/src/main/java/com/orienteer/treasurecreate/ClueTypePhotoCreateFragment.kt
@@ -7,8 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceImageLabelerOptions
 import com.orienteer.camera.CameraUploadOptionDialog
 import com.orienteer.databinding.FragmentCluePhotoCreateBinding
+import timber.log.Timber
+import java.io.IOException
+
 
 class ClueTypePhotoCreateFragment : Fragment(),
     CameraUploadOptionDialog.CameraUploadOptionListener {
@@ -37,5 +43,48 @@ class ClueTypePhotoCreateFragment : Fragment(),
                 .load(uri)
                 .into(binding.photoClueCreateImg)
         }
+        analyzeImageForTags(uri)
+    }
+
+    fun analyzeImageForTags(uri: Uri) {
+        var image: FirebaseVisionImage? = null
+        try {
+            image = FirebaseVisionImage.fromFilePath(context!!, uri)
+        } catch (e: IOException) {
+            Timber.e("Error creating firebase image from URI $uri with message: ${e.message}")
+            e.printStackTrace()
+        }
+
+        // Create instance of the firbase labeler
+        //val labeler = FirebaseVision.getInstance().onDeviceImageLabeler
+
+        //  Or, to set the minimum confidence required:
+        // Switch to cloud image labeler with getCloudImageLabeler()
+        val options =
+            FirebaseVisionOnDeviceImageLabelerOptions.Builder()
+                .setConfidenceThreshold(0.5f)
+                .build()
+        val labeler = FirebaseVision.getInstance()
+            .getOnDeviceImageLabeler(options)
+
+        val tags = StringBuilder()
+        image?.let {
+            labeler.processImage(image)
+                .addOnSuccessListener {
+                    // Task completed successfully
+                    // ...
+                    // TODO: Add these to a recylcerview
+                    for (label in it) {
+                        tags.append("confidence: ${label.confidence} tag: ${label.text} \n")
+                    }
+
+                    binding.tagsTemp.text = tags.toString()
+                }
+                .addOnFailureListener {
+                    // Task failed with an exception
+                    // ...
+                }
+        }
+
     }
 }
