20dfafea6dda7108d9500b32baec0bd593f600c2
jgliwa <jgliwa12@gmail.com>
2020-12-20 20:11:43 -0800

    starting create flow rewrite

---
+++ app/src/main/java/com/orienteer/adventurecreate/fragments/AdvCreateFragment.kt
@@ -0,0 +1,22 @@
+package com.orienteer.adventurecreate.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.orienteer.databinding.FragmentAdvcreateBinding
+
+
+class AdvCreateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentAdvcreateBinding.inflate(inflater)
+
+        return binding.root
+    }
+}
